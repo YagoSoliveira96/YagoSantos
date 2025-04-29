f988fb8af3d8e77a7c885065b08a301590b8a5c8
YagoSoliveira96 <yagocleide123@gmail.com>
2025-04-29 13:16:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector('#botao');
+    botao.style.background='blue'
+
+botao.addEventListener("click",e =>{
+    botao.style.background="blue";
+    botao.innerHTML="Seja bem vindo. Um pouquinho sobre mim!";
+});  
